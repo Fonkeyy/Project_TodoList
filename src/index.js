72e99c36e51f209b940114196a9d90b18a13d7cf
Fonkeyy <cedrick.guevel@gmail.com>
2023-10-30 10:56:17 +0100

    improve checkbox styling

--- src/index.js
+++ src/index.js
@@ -3,29 +3,14 @@ import { ToDoItem } from './JS-files/ToDoClass';
 import { loadMainPage } from './JS-files/homePage';
 import './CSS-files/global.css';
 
-// const defaultProject =
 new Project('default');
-// const testProject =
+new Project('Archive');
 new Project('test');
-// const test1Project =
 new Project('test1');
 
-// const toDoTest =
 new ToDoItem('try mdp 123456', 'find something', '2023-17-08', '1', 'test', 'no comments');
 new ToDoItem('try mdp', 'find something', '2023-07-04', '2', 'default', 'no comments');
 new ToDoItem('trydsfbfbdw mdp', 'find sdbdfb', '2023-07-04', '1', 'default', 'no comments');
 new ToDoItem('try msdfbwdfbbwdffdp', 'ffdwbind something', '2023-07-04', '3', 'default', 'no comments');
-// const toDoTest1 =
-
-// defaultProject.addNewItem(toDoTest1);
-// testProject.addNewItem(toDoTest1);
-// testProject.addNewItem(toDoTest);
-// testProject.addNewItem(toDoTest1);
-// testProject.addNewItem(toDoTest);
-// testProject.addNewItem(toDoTest1);
-// testProject.addNewItem(toDoTest);
-// test1Project.addNewItem(toDoTest);
-// test1Project.addNewItem(toDoTest);
-// test1Project.addNewItem(toDoTest);
 
 loadMainPage();
